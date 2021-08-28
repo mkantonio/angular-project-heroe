fab4eb938a481da4fe8241ff259c75644969222c
Marco Antonio <mkchoque@gmail.com>
2021-08-28 15:55:29 -0400

    M0506 uso de FormsModule

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
-  styleUrls: ['./main-page.component.css']
+  styleUrls: ['./main-page.component.css'],
 })
-export class MainPageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class MainPageComponent {
+  
+  agregar() {
+    console.log('prueba de FormsModule');
   }
-
 }
