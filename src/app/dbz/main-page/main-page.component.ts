8521dde8d8a75b4ab96500381869841b602e7d1e
mkantonio <mkchoque@gmail.com>
2021-10-15 12:33:47 -0400

    M0514 creacion de servicios

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 
 @Component({
@@ -30,5 +31,7 @@ export class MainPageComponent {
     console.log("hola desde el padre");
     this.personajes.push(argumento);
   }
+
+  constructor(private dbzService: DbzService){}
 
 }
